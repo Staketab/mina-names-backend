0dcb9f4295ef3a7a981a3cafed42a31328af9286
Den Streltsov <den.strelts@gmail.com>
2024-02-21 18:54:05 +0700

    first commit

---
+++ src/main/java/com/staketab/minanames/MinaNamesApplication.java
@@ -0,0 +1,13 @@
+package com.staketab.minanames;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MinaNamesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MinaNamesApplication.class, args);
+	}
+
+}
