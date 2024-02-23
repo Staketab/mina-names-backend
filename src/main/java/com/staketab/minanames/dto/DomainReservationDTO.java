c6ab18e1f1072e172dc2739532271bbcdc968524
Den Streltsov <den.strelts@gmail.com>
2024-02-23 17:49:36 +0700

    fix problem with dto serialisable

--- src/main/java/com/staketab/minanames/dto/DomainReservationDTO.java
+++ src/main/java/com/staketab/minanames/dto/DomainReservationDTO.java
@@ -1,10 +1,14 @@
 package com.staketab.minanames.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class DomainReservationDTO {
     private String ownerAddress;
     private String domainName;
